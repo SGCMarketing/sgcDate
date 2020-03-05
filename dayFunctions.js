a3b988a08783782a837d296c230b5a1fd313b6df
Andy Chadbourne <andy@sgcmarketing.co.uk>
2020-03-05 23:41:47 +0000

    1.0.4

--- dayFunctions.js
+++ dayFunctions.js
@@ -4,13 +4,13 @@ module.exports.endOfDay = endOfDay
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function startOfDay(date) {
     //
-    date.setHours(date.getHours() + 1)
+    // date.setHours(date.getHours() + 1)
     return new Date(date.getFullYear(), date.getMonth(), date.getDate(), 00, 00, 00)
 }
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function endOfDay(date) {
     //
-    date.setHours(date.getHours() + 0)
+    // date.setHours(date.getHours() + 0)
     return new Date(date.getFullYear(), date.getMonth(), date.getDate(), 23, 59, 59, 99)
 }
